8923bafd9e6621d2ae23c562e3e40a6e58e5ec28
gnekeng <gnekeng@gmail.com>
2021-07-22 17:43:43 +0700

    initial

---
+++ blog-web/content/authors/pongsathon-cc.md
@@ -0,0 +1,12 @@
+---
+id: pongsathon-cc
+title: 'Pongsathon cc'
+link: 'github.com/6110110296'
+image: 'uploads/author-images/pongsathon_cc.png'
+email: '6110110296@gmail.com'
+blurb: 'Don't bully me.'
+---
+
+# Bio
+
+If you would like your authors to have a bio, welp, it can go right here.
---
+++ blog-web/src/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <Layout pageTitle="About">
+    <section class="section">
+      <div class="container">
+        <div class="content">
+          <h2>About us</h2>
+          <p>
+            I study in Thailand.This work was made for web-application subject.
+          </p>
+        </div>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About me',
+  },
+};
+</script>
