7632bf35510304eb548ba9f4f7fc5b640ecd4290
gnekeng <gnekeng@gmail.com>
2021-07-22 21:08:37 +0700

    test on production

--- blog-web/src/pages/About.vue
+++ blog-web/src/pages/About.vue
@@ -2,6 +2,13 @@
   <Layout pageTitle="About">
     <section class="section">
       <div class="container">
+        
+        <div class="content">
+          <h2>About us</h2>
+          <p>
+            I study in Thailand.This work was made for web-application subject.
+          </p>
+        </div>
         <figure class="image is-rounded is-128x128">
           <img class="is-rounded" :src="`../../uploads/author-images/author-avatar.png`" />
         </figure>
